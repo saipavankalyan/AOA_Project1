3a48893784b34566e307cf5fe24d314abd1d23f9
saipavankalyan <saipavan.munaga@gmail.com>
2023-02-25 20:48:34 -0500

    making sure start and end date shouldn't be same

--- generate_testcases.py
+++ generate_testcases.py
@@ -5,7 +5,7 @@ def generate_testcases(n, m):
     print(n, m)
     times = []
     for i in range(1, m+1):
-        start = random.randrange(0, n+1)
+        start = random.randrange(0, n)
         end = random.randrange(start + 1, n+1)
         times.append((start, end))
     times = sorted(times)
